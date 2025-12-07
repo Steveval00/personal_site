bc30c67efd5b619ae1024b8344dea0b1d7849660
Steveval00 <stefano.valenti2000@gmail.com>
2025-12-07 04:40:47 +0100

    fix: sender

--- src/actions/sendEmail.ts
+++ src/actions/sendEmail.ts
@@ -1,12 +1,21 @@
 "use server";
 
 import { Resend } from "resend";
+
 import { validateString, getErrorMessage } from "@/lib/utils";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
 	const senderEmail = formData.get("senderEmail");
 	const message = formData.get("message");
+
+	const apiKey = process.env.RESEND_API_KEY;
+
+	if (!apiKey) {
+		throw new Error("RESEND_API_KEY non è definita nelle env");
+	}
+
+	const resend = new Resend(apiKey);
 
 	// Validate the server side data
 	if (!validateString(senderEmail, 500)) {
@@ -24,9 +33,9 @@ export const sendEmail = async (formData: FormData) => {
 	let data;
 	try {
 		data = await resend.emails.send({
-			from: "Contact Form  <onboarding@resend.dev>",
+			from: "PORTFOLIO <onboarding@resend.dev>",
 			to: "stefano.valenti2000@gmail.com",
-			subject: "Message from contact form",
+			subject: "Nuovo Messaggio dal Modulo di Contatto Sul Portfolio!",
 			reply_to: senderEmail as string,
 			text: message as string,
 		});
